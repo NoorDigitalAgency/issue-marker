f20101792651320e93b9f6b568d3eb9b602bce78
Rojan Gh <sojaner@gmail.com>
2022-04-25 13:35:37 +0200

    Progress

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,74 @@
-import { getInput, setFailed } from '@actions/core';
+import { debug, endGroup, getInput, startGroup, warning } from '@actions/core';
 import { exec, getExecOutput } from '@actions/exec';
 import { load, dump } from 'js-yaml';
 import { context, getOctokit } from '@actions/github';
 import { EOL } from 'os';
 
 async function run(): Promise<void> {
+
   try {
-    const regex = /^v20[2-3]\d(?:\.\d{1,3}){1,2}(?<stage>-alpha\.\d{1,3}|-beta\.\d{1,3}|)$/;
-    const version = getInput('version');
-    if (!regex.test(version)) throw new Error(`Invalid Version '${version}'.`);
+
+    const productionRegex = /^v20[2-3]\d(?:\.\d{1,3}){1,2}$/i;
+
+    const betaRegex = /^v20[2-3]\d(?:\.\d{1,3}){1,2}-beta\.\d{1,3}$/i;
+
+    const alphaRegex = /^v20[2-3]\d(?:\.\d{1,3}){1,2}-alpha\.\d{1,3}$/i;
+
+    const logRegex = /^([0-9a-f]{40}) Merge pull request #(\d+) from .+?$/m;
+
+    const version = getInput('version', {required: true});
+
+    debug(`Version: '${version}'.`);
+
+    if ([productionRegex, betaRegex, alphaRegex].every(regex => !regex.test(version))) throw new Error(`Invalid Version '${version}'.`);
+
     const previousVersion = getInput('previous_version');
-    if (!regex.test(previousVersion)) throw new Error(`Invalid Previous Version '${version}'.`);
-    const versionStage = regex.exec(version)!.groups!.stage;
-    const token = getInput('token');
+
+    debug(`Previous Version: '${previousVersion}'.`);
+
+    if (previousVersion !== '' && [productionRegex, betaRegex, alphaRegex].every(regex => !regex.test(previousVersion))) throw new Error(`Invalid Previous Version '${previousVersion}'.`);
+
+    if (previousVersion !== '' && [productionRegex, betaRegex, alphaRegex].some(regex => regex.test(version) !== regex.test(previousVersion)))
+
+      throw new Error(`Version '${version}' and Previous Version '${previousVersion}' are from different stages.`);
+
+    const token = getInput('token', {required: true});
+
+    debug(`Token: '${token}'.`);
+
+    const stage = productionRegex.test(version) ? 'production' : betaRegex.test(version) ? 'beta' : alphaRegex.test(version) ? 'alpha' : null;
+
+    debug(`Stage: '${stage}'.`);
+
+    if (typeof (stage) === 'undefined') throw new Error('Problem in detecting the stage.');
+
     const octokit = getOctokit(token);
-    await exec('git', ['fetch', '--all']);
-    await getExecOutput('git', ['log', previousVersion ? `${previousVersion}...${version}` : version, '--reverse', '--merges', '--oneline', '--no-abbrev-commit',  `--grep='Merge pull request #'`]);
+
+    if (stage === 'alpha') {
+
+      await exec('git', ['fetch', '--all']);
+
+      const logOutput = await getExecOutput('git', ['log', previousVersion ? `${previousVersion}...${version}` : version, '--reverse', '--merges', '--oneline', '--no-abbrev-commit',  `--grep='Merge pull request #'`]);
+
+      if (logOutput.exitCode !== 0) throw new Error(logOutput.stderr);
+
+      const log = logOutput.stdout;
+
+      startGroup('Log Output');
+
+      debug(log);
+
+      endGroup();
+
+      const merges = [...log.matchAll(logRegex)];
+
+      if (merges == null || merges.length === 0) throw new Error('No merges found.');
+
+    } else {
+
+
+    }
+
     octokit.graphql('');
     // All the commits
     // git log v1.6.0...v1.7.0 --reverse --merges --oneline
@@ -75,7 +128,7 @@ query PullRequestIssues($owner: String!, $name: String!, $numaber: Int!) {
     */
     const issues = await octokit.rest.search.issuesAndPullRequests({q: ''})
   } catch (error) {
-    if (error instanceof Error) setFailed(error.message);
+    if (error instanceof Error) warning(error.message);
   }
 }
 
